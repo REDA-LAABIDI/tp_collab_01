1cbb087acd1777f6fdf04efa3f537674a1ca70af
unknown <REDA-LAABIDI>
2026-01-15 15:50:33 +0100

    Initial commit

--- tp01.py
+++ tp01.py
@@ -1,1 +1,7 @@
-# Créer une fonction qui renvoie le texte en majuscule
+def mettre_en_majuscule(texte):
+    return texte.upper()
+saisie = input("Entrez le texte à transformer : ")
+resultat = mettre_en_majuscule(saisie)
+print("------------------------------")
+print("Résultat :", resultat)
+print("------------------------------")
